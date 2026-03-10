9b3c907663a36863f049177f9b46038ed1c96557
mehmetturuncx <mehmetturuncx@gmail.com>
2026-03-10 14:17:50 +0300

    Added remove option

--- rebin/cli.py
+++ rebin/cli.py
@@ -2,7 +2,8 @@ import sys
 from .delete import delete_file
 from .restore import restore
 from .list import list_trash
-from .empty import empty_trash  # Bu dosyayı az önce yaptığımız gibi kaydet
+from .empty import empty_trash
+from .remove import remove_file
 
 def print_help():
     print("Rebin - Terminal Trash Tool")
@@ -11,6 +12,7 @@ def print_help():
     print("  python3 rebin.py restore <dosya>  -> Trash'ten geri yükle")
     print("  python3 rebin.py list             -> Trash içeriğini listele")
     print("  python3 rebin.py empty            -> Çöp kutusunu tamamen boşalt")
+    print("  python3 rebin.py remove <dosya>   -> Seçilen dosyayı sil")
 
 def main():
     if len(sys.argv) < 2:
@@ -27,5 +29,7 @@ def main():
         list_trash()
     elif command == "empty":
         empty_trash()
+    elif command == "remove" and len(sys.argv) == 3:
+        remove_file(sys.argv[2])
     else:
         print_help()
